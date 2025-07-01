7a67d656abf58f3cbdfb9b5cd132866b6894512b
Ruben Berenguel Montoro <ruben@mostlymaths.net>
2025-07-01 09:22:46 +0200

    Haptics on Android felt meh

--- haptic.js
+++ haptic.js
@@ -49,9 +49,9 @@ export function initHaptic() {
 
 /**
  * Triggers haptic feedback.
- * @param {number} [duration=10] - Vibration duration in ms for non-iOS devices.
+ * @param {number} [duration=5] - Vibration duration in ms for non-iOS devices.
  */
-export function triggerHaptic(duration = 10) {
+export function triggerHaptic(duration = 5) {
   if (!hapticLabel) {
     console.warn("Haptic feedback not initialized. Call initHaptic() first.");
     return;
@@ -68,7 +68,7 @@ export function triggerHaptic(duration = 10) {
 
 export function triggerHapticError() {
   if (navigator.vibrate) {
-    navigator.vibrate([10, 50, 10]);
+    navigator.vibrate([5, 20, 5]);
   } else {
     triggerHaptic();
     setTimeout(() => triggerHaptic(), 120);
